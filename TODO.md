dc072b0a2201367b36284698d21f3cc0aa7a4564
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2023-09-18 10:04:27 -0600

    Terminada la funcionalidad de enviar correos para agendar citas 1. Se envía la plantilla HTML en lugar del texto de pruebo y el sujeto    respectivo 2. Se añaden funciones auxiliares para esto

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 # Cosas por hacer
 
-- [ ] Modificar el mensaje del correo del servicio de citas
 - [ ] Testear el servicio de Spotify
 - [ ] Documentar el servicio de Spotify
--- src/services/appointmentsService.js
+++ src/services/appointmentsService.js
@@ -29,6 +29,71 @@ const storeAppointment = async (nickname, appointment) => {
   session.close()
 }
 
+const getUser = async (nickname) => {
+  const session = database.session()
+  const query = `
+    MATCH (u:User {nickname: $nickname})
+    RETURN u AS user
+    LIMIT 1
+  `
+
+  const result = await session.run(query, {
+    nickname
+  })
+
+  session.close()
+
+  const [record] = result.records
+  const user = record.get("user").properties
+
+  return user
+}
+
+const formatAppointmentEmailHtmlString = (user, appointment) => {
+  const { studentIdentifier } = user
+  const { description } = appointment
+  const datetime = new Date(appointment.datetime)
+  const htmlString = `
+    <body style="font-family: Arial, sans-serif; background-color: #18072B; margin: 0; padding: 0;">
+        <header style="background-image: linear-gradient(to right,#8C52FF, #FF66CA); border-radius: 0px 0px 20px 20px; text-align: center; padding: 20px 0;">
+            <h1 style="font-size: 40px; color: #ffffff">FOCUS APP</h1>
+        </header>
+        <div style="padding: 20px">
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="center">
+                  <div style="width: 400px; height: auto; margin: 20px; padding: 20px; background-color: #FFF; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(255, 0, 242, 0.2), 0 6px 20px 0 rgba(255, 0, 242, 0.2);">
+                      <h1 style="color: #8C52FF; text-align: center;">Nueva Cita FOCUS APP</h1>
+                      <p style="text-align: center; color: #18072B;">Detalles de la cita:</p>
+                      <ul style="list-style: none;">
+                          <li align="left">Carnet: ${studentIdentifier}</li>
+                          <li align="left">Hora: ${datetime.getHours().toString().padStart(2, "0")}:${datetime.getMinutes().toString().padStart(2, "0")}</li>
+                          <li align="left">Día: ${datetime.getDate()}/${datetime.getMonth() + 1}/${datetime.getFullYear()}</li>
+                          <li align="left">Descripción: ${description}</li>
+                      </ul>
+                  </div>
+                </td>
+              </tr>
+            </table>
+        </div>
+        <footer style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); background-image: linear-gradient(to right,#8C52FF, #FF66CA); color: #FFF; text-align: center; padding: 10px 0;">
+            <br>
+            &copy; FocusApp
+            <p>Cedes Don Bosco</p>
+        </footer>
+    </body>
+  `
+
+  return htmlString
+}
+
+const formatAppointmentSubject = (user) => {
+  const { name, firstSurname, secondSurname } = user
+  const subject = `Cite agendada por FOCUS APP para ${name} ${firstSurname} ${secondSurname}`
+
+  return subject
+}
+
 const bookAppointment = async (nickname, appointment) => {
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.sendinblue.com",
@@ -43,11 +108,13 @@ const bookAppointment = async (nickname, appointment) => {
     }
   })
 
+  const user = await getUser(nickname)
+
   const result = await transporter.sendMail({
     from: `"Sistema de agendado de citas de Focus" ${process.env.FOCUS_APPOINTMENT_BOOKER_EMAIL}`,
     to: process.env.FOCUS_SPECIALIST_EMAIL,
-    subject: "Hola",
-    text: "Texto de prueba",
+    subject: formatAppointmentSubject(user),
+    html: formatAppointmentEmailHtmlString(user, appointment)
   })
 
   return result
